34be45a6bbb650ee84c65c0bf2710a5a643d65e8
hamanon <physicsnoopy.k.o@gmail.com>
2018-05-19 11:26:54 +0900

    repair max_iter

--- bin/exe_GaussianMixture.jl
+++ bin/exe_GaussianMixture.jl
@@ -24,18 +24,18 @@ function test()
     gmm = GaussianMixtureModel.sample_GMM(bgmm)
     X, S = GaussianMixtureModel.sample_data(gmm,N)
 
-    max_iter = 10
-    gmm,last_ELBO,gmm_best,ELBO_best = GaussianMixtureModel.learn_em(X, K, max_iter)
+    max_iter = 100
+    gmm_fit,last_ELBO,gmm_best,ELBO_best = GaussianMixtureModel.learn_em(X, K, max_iter)
     println(gmm_best.Gcmp[1].mu)
     println(gmm_best.Gcmp[2].mu)
     println(gmm_best.Gcmp[3].mu)
     println(gmm_best.Gcmp[4].mu)
     println(gmm_best.Gcmp[5].mu)
-    return X, gmm,gmm_best
+    return X, gmm_fit, gmm_best
 end
 
-X,gmm,gmm_best=test()
-scatter(X[1,:],X[2,:],title="test"); savefig("test.png")
+X, gmm, gmm_best = test()
+scatter(X[1,:],X[2,:],title="test"); savefig("pic/test.png")
 mus=[gmm.Gcmp[1].mu[1] gmm.Gcmp[1].mu[2];
      gmm.Gcmp[2].mu[1] gmm.Gcmp[2].mu[2];
      gmm.Gcmp[3].mu[1] gmm.Gcmp[3].mu[2];
@@ -47,8 +47,9 @@ mus2=[gmm_best.Gcmp[1].mu[1] gmm_best.Gcmp[1].mu[2];
       gmm_best.Gcmp[4].mu[1] gmm_best.Gcmp[4].mu[2];
       gmm_best.Gcmp[5].mu[1] gmm_best.Gcmp[5].mu[2]]
 scatter!(mus[:,1],mus[:,2])
+savefig("pic/test2.png")
 scatter!(mus2[:,1],mus2[:,2])
-savefig("test4.png")
+savefig("pic/test3.png")
 
 
 
